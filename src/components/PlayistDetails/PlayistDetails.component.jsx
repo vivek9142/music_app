548dc8c873eba7c0aa19f409ff59f34d414e0b6f
vivek9142 <vm199841@gmail.com>
2021-12-26 20:12:37 +0530

    added shuffle play feature

--- src/components/PlayistDetails/PlayistDetails.component.jsx
+++ src/components/PlayistDetails/PlayistDetails.component.jsx
@@ -1,39 +1,46 @@
 import { useState } from 'react';
 import { Button,Typography } from '@material-ui/core';
-import { useSelector,useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import SongsList from '../SongsList/SongsList.component';
 import FormComponent from '../Form/Form.component';
 
-const PlayistDetails = (props) => {
-    const dispatch = useDispatch();
+const PlayistDetails = ({data,...props}) => {
     const [search,setSearch] = useState('');
     const [addSongComp,showAddSongComp] = useState(false);
     const finalData = useSelector(state => state.data.finalData);
-    
+    const [origData,setOrigData] = useState(data.songs);
+
     const handleFormChange = (val) => {
         setSearch(val);   
     }
-
+    const shuffleHandler = () => {
+        for(let i=data.songs.length-1;i>0;i--){
+            let j = Math.floor(Math.random()*(i+1));
+            [data.songs[i],data.songs[j]] = [data.songs[j],data.songs[i]];
+        }
+        console.log(data.songs);
+        setOrigData([...data.songs]);
+    }
     return (
         <>
         <div className="playist_container">
-            <Typography variant='h3'>{props.data.name}</Typography>
+            <Typography variant='h3'>{data.name}</Typography>
             {
                 addSongComp ? 
                 (<>
                 <FormComponent searchForm onChange={handleFormChange}/>
-                <SongsList addtoList data={finalData} search={search} playistId={props.data.id}/>
+                <SongsList addtoList data={finalData} search={search} playistId={data.id}/>
                 </>) 
                 :(
                 <div className="playist__container__body">
                     <div className="playist__container__body_buttons">
-                        <Button>Shuffle Play</Button>
+                        <Button onClick={shuffleHandler}>Shuffle Play</Button>
                         <Button onClick={()=> showAddSongComp(prevState=> !prevState.addSongComp)}>Add Song</Button>
                     </div>
                 
                     <div className="songs_container">
-                        {props.data.songs.length>0 ?
-                        (<SongsList data={props.data.songs} playistId={props.data.id}/>)
+                        {data.songs.length>0 ?
+                        (<SongsList data={origData} playistId={data.id}/>)
                         :(<Typography variant='h6' component='p'>No Songs Found</Typography >)}
                     </div>
                 </div>
